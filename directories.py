74a012ccccacfc0c49ee85b14f73d7984b049dab
Sana Tauqir <85200846+sanatauqir92@users.noreply.github.com>
2025-02-20 11:46:29 -0800

    final stopping point
    completed partial move operation, the deeper levels of move and the delete operation were not implemented

--- directories.py
+++ directories.py
@@ -1,77 +1,36 @@
-#input:
-# CREATE fruits
-# CREATE vegetables
-# CREATE grains
-# CREATE fruits/apples
-# CREATE fruits/apples/fuji
-# LIST
-# CREATE grains/squash
-# MOVE grains/squash vegetables
-# CREATE foods
-# MOVE grains foods
-# MOVE fruits foods
-# MOVE vegetables foods
-# LIST
-# DELETE fruits/apples
-# DELETE foods/fruits/apples
-# LIST
-
-#output:
-# CREATE fruits
-# CREATE vegetables
-# CREATE grains
-# CREATE fruits/apples
-# CREATE fruits/apples/fuji
-# LIST
-# fruits
-#   apples
-#     fuji
-# grains
-# vegetables
-# CREATE grains/squash
-
-#-------------------
-# MOVE grains/squash vegetables
-# CREATE foods
-# MOVE grains foods
-# MOVE fruits foods
-# MOVE vegetables foods
-# LIST
-# foods
-#   fruits
-#     apples
-#       fuji
-#   grains
-#   vegetables
-#     squash
-# DELETE fruits/apples
-# Cannot delete fruits/apples - fruits does not exist
-# DELETE foods/fruits/apples
-# LIST
-# foods
-#   fruits
-#   grains
-#   vegetables
-#     squash
-
 class Node:
   def __init__(self, name):
     self.name = name
     self.children = []
 
-#add node, find node, delete node, move node, print all
 class Tree:
   def __init__(self, root_data):
     self.root = Node(root_data)
 
   def add_child(self, parent_data, child_data):
     parent_node = self._find_node(self.root, parent_data)
-    #ideally instead of sorting the list each time, I would add each child node in sorted order
+    
     if parent_node:
         parent_node.children.append(Node(child_data))
         parent_node.children.sort(key=lambda x:x.name)
     else:
         raise ValueError("Parent node not found")
+    
+  def remove_child(self, parent_name, child_name):
+    if not parent_name:
+      return None
+    
+    parent_node = self._find_node(self.root, parent_name)
+
+    if not parent_node.children and parent_name == child_name:
+      return None
+  
+    if parent_node.children:
+      parent_node.children = [
+        child for child in parent_node.children if child.name != child_name
+      ]
+    else:
+      raise ValueError("Parent node not found")
   
   def _find_node(self, node, name):
     if node.name == name:
@@ -97,16 +56,26 @@ CREATE grains
 CREATE fruits/apples
 CREATE fruits/apples/fuji
 LIST
-CREATE grains/squash"""
+CREATE grains/squash
+MOVE grains/squash vegetables
+CREATE foods
+MOVE grains foods
+MOVE fruits foods
+MOVE vegetables foods
+LIST
+DELETE fruits/apples
+DELETE foods/fruits/apples
+LIST"""
 
 def parse_input(input):
   directoryTree = Tree('root')
   for line in input.splitlines():
     print(line)
-    choice = line.split()[0]
-    new_directory = line.split()[1] if (len(line.split()) > 1) else ""
+    s_line = line.split()
+    choice = s_line[0]
     match choice:
       case "CREATE":
+        new_directory = s_line[1] if (len(s_line) > 1) else ""
         if "/" in new_directory:
           split = new_directory.split("/")
           directoryTree.add_child(split[len(split)-2], split[len(split)-1])
@@ -115,12 +84,17 @@ def parse_input(input):
       case "LIST":
         directoryTree.display()
       case "MOVE":
-        print(choice)
-      case "DELETE":
-        print(choice)
+        out_directory = ""
+        clean_directory = ""
+        if "/" in s_line[1]:
+          split = s_line[1].split("/")
+          out_directory = split[len(split)-1]
+          clean_directory = split[0]
+          in_directory = s_line[2]
+          directoryTree.add_child(in_directory, out_directory)
+          directoryTree.remove_child(clean_directory, out_directory)
       case _:
-        print('nope')
-
+        return
 
 parse_input(input)
     
